98247af2abd6c8093b17f8a0057035ddf175c2b9
jsepulveda2 <jsepulveda0828@gmail.com>
2025-06-13 15:53:34 -0400

    refactor: update Solidity pragma version and reorganize Migrations contract for clarity

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,19 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 
 contract Migrations {
-  address public owner = msg.sender;
-  uint public last_completed_migration;
+    address public owner;
 
-  modifier restricted() {
-    require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner"
-    );
-    _;
-  }
+    uint public last_completed_migration;
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier restricted() {
+        require(
+            msg.sender == owner,
+            "This function is restricted to the contract's owner"
+        );
+        _;
+    }
+
+    function setCompleted(uint completed) public restricted {
+        last_completed_migration = completed;
+    }
 }
