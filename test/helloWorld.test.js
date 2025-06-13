675bf6edf95073b8319c638064c31eed59b54a20
jsepulveda2 <jsepulveda0828@gmail.com>
2025-06-13 15:54:58 -0400

    test: add initial Hello World test case

---
+++ test/helloWorld.test.js
@@ -0,0 +1,6 @@
+const assert = require('assert');
+describe('Hello World Test', () => {
+    it('should return hello world', () => {
+        assert.strictEqual('hello world', 'hello world');
+    });
+});
