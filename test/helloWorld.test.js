023bdfe3ae790f943a8972674a9de0e684f9b5d8
jsepulveda2 <jsepulveda0828@gmail.com>
2025-06-13 16:06:07 -0400

    current

--- test/helloWorld.test.js
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-describe('Hello World Test', () => {
-    it('should return hello world', () => {
-        assert.strictEqual('hello world', 'hello world');
-    });
-});
