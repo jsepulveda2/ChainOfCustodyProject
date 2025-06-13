24a57185cafb7fdf2f9491e3c0e554c9600432fd
jsepulveda2 <jsepulveda0828@gmail.com>
2025-06-13 15:11:11 -0400

    commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   solidity: "0.8.10",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8546"
+      url: "http://127.0.0.1:8545"
     },
     hardhat: {
       // See its defaults
